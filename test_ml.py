06ce46bb116f6c68c29fab40b749b33cf8529950
William Lio <liowilliam0@gmail.com>
2025-09-14 21:52:17 -0400

    Added unit tests and screenshot of tests running

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,60 @@
 import pytest
-# TODO: add necessary import
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
+from ml.data import process_data
+from ml.model import train_model, inference, compute_model_metrics
+
+data = pd.DataFrame({
+    "age": [19, 28, 39,],
+    "workclass": ["Private", "Self-emp", "Sales"],
+    "education": ["Bachelors", "HS-grad", "Masters"],
+    "salary": [0,0,1,]
+})
+
+cat_features = ["workclass", "education"]
+
+def test_process_dtype():
     """
-    # add description for the first test
+    Tests if process_data returns correct data types
     """
-    # Your code here
-    pass
+    X, y, encoder, lb = process_data(
+        data, categorical_features=cat_features, label="salary", training=True
+    )
+
+    assert isinstance(X, np.ndarray)
+    assert isinstance(y, np.ndarray)
+    assert encoder is not None
+    assert lb is not None
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_train_model():
     """
-    # add description for the second test
+    Test if model returns a RandomForestClassifier
     """
-    # Your code here
-    pass
+    X, y, encoder, lb = process_data(
+        data, categorical_features=cat_features, label="salary", training=True
+    )
+
+    model = train_model(X,y)
+    
+    assert isinstance(model, RandomForestClassifier)
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_model_metrics():
     """
-    # add description for the third test
+    Test if compute_model_metrics returns floats and reasonable values
     """
-    # Your code here
-    pass
+    y_true = np.array([0,1,0,1])
+    y_pred = np.array([0,1,0,0])
+
+    precision, recall, fbeta = compute_model_metrics(y_true, y_pred)
+
+    assert isinstance(precision, float)
+    assert isinstance(recall, float)
+    assert isinstance(fbeta, float)
+
+    assert 0.0 <= precision <= 1.0
+    assert 0.0 <= recall <= 1.0
+    assert 0.0 <= fbeta <= 1.0
